c8d1fe25a8e47f084aa1a304a5352e9d3e8160e6
melvinzhang <melvinzhang@gmail.com>
2019-08-18 16:36:55 +0800

    use cffi to manipulate global variable

--- cffi.lua
+++ cffi.lua
@@ -2,8 +2,12 @@ local ffi = require("ffi")
 ffi.cdef[[
 int addc(int a, int b);
 int increment(int c);
+int G;
 ]]
 print(ffi.os)
 print(ffi.C.addc(1, 2))
 print(ffi.C.increment(5))
 print(ffi.C.increment(10))
+print(ffi.C.G)
+ffi.C.G = 1
+print(ffi.C.increment(5))
