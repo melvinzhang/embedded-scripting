eb1828a310a7040397081565561bbc8f85630bb5
melvinzhang <melvinzhang@gmail.com>
2019-08-19 21:22:41 +0800

    create local G for ffi.C

--- cffi.lua
+++ cffi.lua
@@ -1,13 +1,20 @@
 local ffi = require("ffi")
+
 ffi.cdef[[
 int addc(int a, int b);
 int increment(int c);
 int G;
 ]]
+
+local G = ffi.C
+
 print(ffi.os)
-print(ffi.C.addc(1, 2))
-print(ffi.C.increment(5))
-print(ffi.C.increment(10))
-print(ffi.C.G)
-ffi.C.G = 1
-print(ffi.C.increment(5))
+print(G.addc(1, 2))
+print(G.increment(5))
+print(G.increment(10))
+
+print(G.G)
+G.G = 4/5
+print(G.G)
+
+print(G.increment(5))
